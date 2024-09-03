07a9d16fe3e66e59655614af5c1619bb349f3e14
peterlindre <peterlindre@gmail.com>
2024-09-03 21:00:11 +0300

    Kodutöö1

---
+++ html/src/App.js
@@ -0,0 +1,65 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+
+
+      
+      <button className="try"> Try it </button>
+      <p className='tekst'>Proov</p>
+      <p className='tekst2'> Proov2</p>
+      <img className='pilt' src="https://onoff.ee/2508228-superlarge_default_2x/apple-iphone-15-pro-128gb-sinine-titanium.jpg" alt='pilt' />
+
+<div>
+      <table>
+        
+  <tr>
+    <th className='rida1'>Company</th>
+    <th className='rida2'>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td >Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+</div>
+<a href="https://www.youtube.com/watch?v=e62D-aayveY&list=PLP9IO4UYNF0VdAajP_5pYG-jG2JRrG72s&index=14"> Vaata rohkem siit</a>
+    </div>
+
+  
+    
+  );
+}
+
+
+export default App;
