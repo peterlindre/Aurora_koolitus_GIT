da513978f0ffe6edd8e412bf166c7340e2121229
peterlindre <peterlindre@gmail.com>
2024-09-03 16:00:10 +0300

    routing

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom" ;
+import Avaleht from './pages/Avaleht';
+import Kinkekaart from './pages/Kinkekaart';
+import Esindused from './pages/Esindused';
+import Ostukorv from './pages/Ostukorv';
+import LisaToode from './pages/LisaToode';
+import Seaded from './pages/Seaded';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <Link to='avaleht'>
+      <img className="pilt" src="https://digi.geenius.ee/app/uploads/sites/7/2020/05/4e8cd4ef7f6f6a06695160e1b2d37c23-1920x1280.jpg" alt="pilt" />
+      </Link>
+      <Link to='osta-kinkekaart'>
+        <button className="nupp">kinkekaart</button>
+      </Link>
+      <Link to='esindused'>
+        <button className="nupp">esindused</button>
+      </Link>
+      <Link to='ostukorv'>
+        <button className="nupp">ostukorv</button>
+      </Link>
+      <Link to='lisa-toode'>
+        <button className="nupp">lisa-tode</button>
+      </Link>
+      <Link to='seaded'>
+        <button className="nupp">seaded</button>
+      </Link>
+
+
+      
+    
+      <Routes>
+        <Route path='avaleht' element={ <Avaleht/>} />
+        <Route path='osta-kinkekaart' element={ <Kinkekaart/>} />
+        <Route path='esindused' element={ <Esindused/>} />
+        <Route path='ostukorv' element={ <Ostukorv/>} />
+        <Route path='lisa-toode' element={ <LisaToode/>} />
+        <Route path='seaded' element={ <Seaded/>} />
+        
+      </Routes>
     </div>
   );
 }
--- veebipood/src/index.js
+++ veebipood/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom" ;
+// kui on bracketid siis votan yhe tyki
+// kui ei ole, siis votan terve mooduli
+// ./ siis on "src" kaustast
+// kui ei ole, siis on "node_module" kaustast
+
+//1. npm i react-router-dom
+//2. import {BrowserRouter} from "react-router-dom" 
+//3. <BrowserRouter> ymbritseb <app/>
+//4. App.js failis teitama URLi ja HTMLi vahelisi seoseid
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
+
   </React.StrictMode>
 );
 
